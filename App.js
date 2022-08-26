43cd55e5aeb60ae241f0718e58d96b865fb41feb
kadirnurkovic <kadir.nurkovic0404@gmail.com>
2022-08-26 19:30:54 +0200

    React hooks

--- App.js
+++ App.js
@@ -1,14 +1,40 @@
 import React from 'react'
 import "./App.css"
-import WeatherCard from './components/Weather/WeatherCard'
-
+import { useState, useEffect } from 'react'
 
 function App(){
-  return (
-   <div className='card-container'>
-      <WeatherCard description/>
-   </div>
-  )
+
+  const [number, setNumber] = useState(0);
+  const [user, setUser] = useState ({})
+
+  const getUser = (id) => {
+      fetch(`https://jsonplaceholder.typecode.com/${id}`)
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json)
+        setUser(json)
+      })
+  }
+
+  useEffect(() => {
+    getUser()
+  }, [])
+
+  const myFunc = () => {
+    console.log("Moja funkcija u useEffect")
+  }
+
+    return (
+      <div className='card-container'>
+        <strong onClick={() => setNumber(Math.random())}>
+          Ovo se nalazi u APP JS
+          <hr/>
+          {number}
+        </strong>
+        {user.title}
+        <button onClick={() => getUser(10)}>Change name</button>
+      </div>
+    )
 };
 
 export default App;
--- components/Weather/WeatherCard.js
+++ components/Weather/WeatherCard.js
@@ -5,7 +5,7 @@ import { useState } from 'react'
 export default function WeatherCard({ description }) {
 
     const [celsius, setCelsius] = useState(Math.floor(Math.random() * 60) - 20);
-    function celsiusChange(){
+        function celsiusChange(){
         setCelsius(Math.floor(Math.random() * 60) - 20)
     }
     if(celsius > -10 && celsius < 33){
@@ -17,7 +17,7 @@ export default function WeatherCard({ description }) {
     }
     return (
         <div className='weather-card' style={{
-            background: celsius > 23 ? 'rgba(255 ,0 ,0 , 0.5)' : celsius > 4 ? 'rgba(255 ,255 ,0 , 0.5)' : 'rgba(0 ,0 ,255 , 0.5)'
+            background: celsius > 23 ? 'rgba(220 ,20 ,60 )' : celsius > 4 ? 'rgb(255,215,0)' : 'rgb(70,130,180)'
         }}>
             <p className='degree'>{celsius}°</p>
             <button className='change-temp' onClick={celsiusChange}>Change temp</button>
